a15d35f3b0410a3d4df8731f4c999f05bb5dd0bb
isaac <shifdub@gmail.com>
2020-10-02 10:27:17 -0600

    added image fragment class

--- app/src/main/java/edu/cnm/deepdive/animals/service/AnimalService.java
+++ app/src/main/java/edu/cnm/deepdive/animals/service/AnimalService.java
@@ -1,6 +1,20 @@
 package edu.cnm.deepdive.animals.service;
 
+import edu.cnm.deepdive.animals.model.Animal;
+import edu.cnm.deepdive.animals.model.ApiKey;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
 public interface AnimalService {
 
+  @GET("getKey")
+  Call<ApiKey> getApiKey();
 
+  @FormUrlEncoded
+  @POST("getAnimals")
+  Call<List<Animal>> getAnimals(@Field("key") String key);
 }
