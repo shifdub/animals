16dbe52ac7afae8bf2647220ce85f9bd7721a8f4
isaac <shifdub@gmail.com>
2020-10-12 16:14:25 -0600

    adjusted spinner layout and color

--- app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
@@ -1,46 +1,48 @@
 
 package edu.cnm.deepdive.animals.controller;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import edu.cnm.deepdive.animals.BuildConfig;
 import edu.cnm.deepdive.animals.R;
 import edu.cnm.deepdive.animals.model.Animal;
-import edu.cnm.deepdive.animals.model.ApiKey;
-import edu.cnm.deepdive.animals.service.AnimalService;
 import edu.cnm.deepdive.animals.viewmodel.AnimalViewModel;
-import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
-public class ImageFragment extends Fragment {
+public class ImageFragment extends Fragment implements OnItemSelectedListener {
 
   private WebView contentView;
   private AnimalViewModel animalViewModel;
+  private Spinner spinner;
+  private List<Animal> animals;
+  private Toolbar toolbar;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     View root = inflater.inflate(R.layout.fragment_image, container, false);
     setupWebView(root);
+    toolbar = root.findViewById(R.id.toolbar);
+    toolbar.setTitle(R.string.app_name);
+    spinner = root.findViewById(R.id.animals_spinner);
+    spinner.setOnItemSelectedListener(this);
+    ((MainActivity) getActivity()).setSupportActionBar(toolbar);
     return root;
   }
 
@@ -48,15 +50,19 @@ public class ImageFragment extends Fragment {
   public void onViewCreated(@NonNull View view,
       @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    animalViewModel = new ViewModelProvider(getActivity())
+    animalViewModel = new ViewModelProvider(Objects.requireNonNull(getActivity()))
         .get(AnimalViewModel.class);
     animalViewModel.getAnimals().observe(getViewLifecycleOwner(), new Observer<List<Animal>>() {
       @Override
       public void onChanged(List<Animal> animals) {
-        contentView.loadUrl(animals.get(22).getImageUrl());
+        ImageFragment.this.animals = animals;
+        ArrayAdapter<Animal> adapter = new ArrayAdapter<>(
+            ImageFragment.this.getContext(), R.layout.custom_spinner_item, animals);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
       }
     });
-
   }
 
   private void setupWebView(View root) {
@@ -74,56 +80,15 @@ public class ImageFragment extends Fragment {
     settings.setDisplayZoomControls(false);
     settings.setUseWideViewPort(true);
     settings.setLoadWithOverviewMode(true);
-    new RetrieverTask().execute();
   }
 
-  private class RetrieverTask extends AsyncTask<Void, Void, List<Animal>> {
+  @Override
+  public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+    contentView.loadUrl(animals.get(pos).getImageUrl());
+  }
 
-    private AnimalService animalService;
+  @Override
+  public void onNothingSelected(AdapterView<?> adapterView) {
 
-    @Override
-    protected void onPreExecute() {
-      super.onPreExecute();
-      Gson gson = new GsonBuilder()
-          .create();
-      Retrofit retrofit = new Retrofit.Builder()
-          .baseUrl(BuildConfig.BASE_URL)
-          .addConverterFactory(GsonConverterFactory.create(gson))
-          .build();
-      animalService = retrofit.create(AnimalService.class);
-    }
-
-    @Override
-    protected List<Animal> doInBackground(Void... voids) {
-      try {
-        Response<ApiKey> keyResponse = animalService.getApiKey().execute();
-        ApiKey key = keyResponse.body();
-        assert key != null;
-        final String clientKey = key.getKey();
-
-        Response<List<Animal>> listResponse = animalService.getAnimals(clientKey).execute();
-        List<Animal> animalList = listResponse.body();
-        assert animalList != null;
-        return animalList;
-      } catch (
-          IOException e) {
-        Log.e("AnimalService", e.getMessage(), e);
-        cancel(true);
-      }
-      return null;
-    }
-
-    @Override
-    protected void onPostExecute(List<Animal> animalList) {
-
-
-      Objects.requireNonNull(getActivity()).runOnUiThread(new Runnable() {
-        @Override
-        public void run() {
-
-
-        }
-      });
-    }
   }
 }
