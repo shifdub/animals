1485552fe37666c7c672424eaea600823470e6e9
isaac <shifdub@gmail.com>
2020-10-19 12:09:57 -0600

    replaced webview W/imageview

---
+++ app/src/main/java/edu/cnm/deepdive/animals/AnimalsApplication.java
@@ -0,0 +1,20 @@
+package edu.cnm.deepdive.animals;
+
+import android.app.Application;
+import com.squareup.picasso.Picasso;
+
+public class AnimalsApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    Picasso.setSingletonInstance(
+        new Picasso.Builder(this)
+            .loggingEnabled(true)
+            .build()
+
+    );
+  }
+
+
+}
