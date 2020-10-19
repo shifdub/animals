0afbc190ec60931ba04fdca58fb52aa4552f5600
isaac <shifdub@gmail.com>
2020-10-19 12:09:57 -0600

    replaced webview W/imageview
    Also made changes to save data during orientation config chamge

--- app/src/main/java/edu/cnm/deepdive/animals/viewmodel/AnimalViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/animals/viewmodel/AnimalViewModel.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class AnimalViewModel extends AndroidViewModel {
 
   private final MutableLiveData<List<Animal>> animals;
+  private final MutableLiveData<Integer> selectedItem;
   private final MutableLiveData<Throwable> throwable;
   private final AnimalService animalService;
 
@@ -22,6 +23,7 @@ public class AnimalViewModel extends AndroidViewModel {
       @NonNull Application application) {
     super(application);
     animals = new MutableLiveData<>();
+    selectedItem = new MutableLiveData<>();
     throwable = new MutableLiveData<>();
     animalService = AnimalService.getInstance();
     loadAnimals();
@@ -29,6 +31,14 @@ public class AnimalViewModel extends AndroidViewModel {
 
   public LiveData<List<Animal>> getAnimals() {
     return animals;
+  }
+
+  public LiveData<Integer> getSelectedItem() {
+    return selectedItem;
+  }
+
+  public void select(int index) {
+    selectedItem.setValue(index);
   }
 
   public LiveData<Throwable> getThrowable() {
